19aa3ae01b3f090ec6742c1ce3abe2e8a420809e
RivalFauzan <rfauzanj31@gmail.com>
2026-06-08 00:53:09 +0700

    Add playGame function and restructure game to run 5 rounds
    - Create playGame function as the main entry point
    - Move score variables and playRound inside playGame for proper scoping
    - Implement logic to call playRound 5 times with fresh inputs each round

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-let humanScore = 0;
-let computerScore = 0;
-let inputPrompt = prompt("batu/gunting/kertas?");
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  let round = 0;
 
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("draw both are : " + humanChoice);
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      console.log("You win! " + humanChoice + " beats " + computerChoice);
+      humanScore++;
+    } else {
+      console.log("You lose " + computerChoice + " beats " + humanChoice);
+      computerScore++;
+    }
+  }
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection);
+}
+
+//loops untuk 5 ronde
+for (i = 1; i <= 5; i++) {
+  playGame();
+}
+
+//function mengambil input random
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -18,32 +46,12 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+  let inputPrompt = prompt("batu/gunting/kertas?");
   if (inputPrompt.toLowerCase() === "batu") {
     return "rock";
   } else if (inputPrompt.toLowerCase() === "gunting") {
     return "scissors";
   } else {
     return "paper";
-  }
-}
-
-let humanSelection = getHumanChoice();
-let compuSelection = getComputerChoice();
-
-playRound(humanSelection, compuSelection);
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("draw both are : " + humanChoice);
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    console.log("You win! " + humanChoice + " beats " + computerChoice);
-    humanScore++;
-  } else {
-    console.log("You lose " + computerChoice + " beats " + humanChoice);
-    computerScore++;
   }
 }
