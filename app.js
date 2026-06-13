8c02a33a60c2473c9b1d10c5cb63f3c114d9bf87
RivalFauzan <rfauzanj31@gmail.com>
2026-06-13 18:37:44 +0700

    change the input prompt with 3 button and add a p that contain round result

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  let round = 0;
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+let draw = 0;
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      console.log("draw both are : " + humanChoice);
-    } else if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "scissors" && computerChoice === "paper") ||
-      (humanChoice === "paper" && computerChoice === "rock")
-    ) {
-      console.log("You win! " + humanChoice + " beats " + computerChoice);
-      humanScore++;
-    } else {
-      console.log("You lose " + computerChoice + " beats " + humanChoice);
-      computerScore++;
-    }
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  if (humanChoice === computerChoice) {
+    console.log("draw both are : " + humanChoice);
+    draw++;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+    humanScore++;
+  } else {
+    console.log("You lose " + computerChoice + " beats " + humanChoice);
+    computerScore++;
   }
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
+  score.textContent = `human score:  ${humanScore} computer score: ${computerScore} draw: ${draw}`;
+  if (humanScore === 5) {
+    score.textContent = "selamat anda menang 5 ronde";
+    resetScore();
+  } else if (computerScore === 5) {
+    score.textContent = "maaf anda kalah 5 ronde";
+    resetScore();
+  } else if (draw === 5) {
+    score.textContent = "anda seri 5 ronde";
+    resetScore();
+  }
 }
 
+function resetScore() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  draw = 0;
+}
 //loops untuk 5 ronde
-for (i = 1; i <= 5; i++) {
-  playGame();
-}
+// for (i = 1; i <= 5; i++) {
+//   playGame();
+// }
 
 //function mengambil input random
 function getRandomInt(max) {
@@ -45,13 +59,23 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let inputPrompt = prompt("batu/gunting/kertas?");
-  if (inputPrompt.toLowerCase() === "batu") {
-    return "rock";
-  } else if (inputPrompt.toLowerCase() === "gunting") {
-    return "scissors";
-  } else {
-    return "paper";
-  }
-}
+// function getHumanChoice() {
+//   // let inputPrompt = prompt("batu/gunting/kertas?");
+//   // if (inputPrompt.toLowerCase() === "batu") {
+//   //   return "rock";
+//   // } else if (inputPrompt.toLowerCase() === "gunting") {
+//   //   return "scissors";
+//   // } else {
+//   //   return "paper";
+//   // }
+//   if ()
+// }
+
+const batu = document.querySelector(".batuBtn");
+const gunting = document.querySelector(".guntingBtn");
+const kertas = document.querySelector(".kertasBtn");
+let score = document.querySelector(".score");
+
+batu.addEventListener("click", () => playRound("rock"));
+gunting.addEventListener("click", () => playRound("scissors"));
+kertas.addEventListener("click", () => playRound("paper"));
